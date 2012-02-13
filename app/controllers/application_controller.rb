c054a2ebecf57d4552e991445ea7e2fa586bfcd7
unknown <filip@.(none)>
2012-02-13 00:13:03 +0000

    messing around with naiveradio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :debug_flag
+
+  def debug_flag
+    unless params[:debug].nil? then
+      session[:debug] = params[:debug]
+    end
+    session[:debug]
+  end
 end
